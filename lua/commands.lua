90abf3f06886de1134686a8a9eae8e3f03e371dc
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-01-28 00:26:19 +0100

    [#10] Correctly detect root markings

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,24 +1,28 @@
 local function setup()
-	vim.api.nvim_create_user_command("TeaConnect", function()
-		local server_address = vim.fn.input("Please enter the server address: ")
-		local language_name = vim.fn.input("Please enter the language name: ")
+    local root_patterns = { ".git", ".clang-format", "pyproject.toml", "setup.py" }
 
-		local host, port = server_address:match("([^:]+):([^:]+)")
-		local config = {
-			host = host,
-			port = port,
-			name = language_name,
-			root_dir = vim.fn.getcwd(),
-			filetypes = { language_name },
-		}
+    vim.api.nvim_create_user_command("TeaConnect", function()
+        local server_address = vim.fn.input("Please enter the server address: ")
+        local language_name = vim.fn.input("Please enter the language name: ")
 
-		require("lsp").setup(config)
-		require("lsp").start()
+        local root_dir = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true })[1])
 
-		print("\nConnection successfull!")
-	end, {})
+        local host, port = server_address:match("([^:]+):([^:]+)")
+        local config = {
+            host = host,
+            port = port,
+            name = "Tea " .. language_name .. " LSP Helper",
+            root_dir = root_dir,
+            filetypes = { language_name },
+        }
+
+        require("lsp").setup(config)
+        require("lsp").start()
+
+        print("\nConnection successfull!")
+    end, {})
 end
 
 return {
-	setup = setup,
+    setup = setup,
 }
